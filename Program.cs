a08359d67a80adc098e6fdb0332109be3eb9f6b6
AleKerezsi <alexandra.kerezsi.18@gmail.com>
2022-12-05 22:57:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        /* Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+		citit de la tastatura*/ 
+		
+		Console.WriteLine("Introduceti n");
+		int n= int.Parse(Console.ReadLine());
+		
+		int sumaCifrelor = 0;
+		
+		while(n>0)
+		{
+			int ultimaCifra = n%10;
+			n= n/10;
+			sumaCifrelor = sumaCifrelor+ ultimaCifra;
+		}
+			Console.WriteLine("Suma cifrelor este "+ sumaCifrelor);
+        }
+    }
+}
